a34535d48db6c2ef24402249ec974e57c8711d40
zhangshuaining <1208505019@qq.com>
2024-04-10 09:44:57 +0800

    feat: add Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
-import Navbar from "./components/Navbar"
-import Hero from "./components/Hero"
-import HomeCards from "./components/HomeCards"
-import JobListings from "./components/JobListings"
-import ViewAllJobs from "./components/ViewAllJobs"
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom'
+import HomePage from "./pages/HomePage"
+import MainLayouts from './layouts/MainLayouts'
+import JobsPage from './pages/JobsPage'
 
 const App = () => {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<MainLayouts />}>
+        <Route index element={<HomePage />} />
+        <Route path='jobs' element={<JobsPage />} />
+      </Route>
+    )
+  )
   return (
-    <>
-      <Navbar />
-      <Hero />
-      <HomeCards />
-      <JobListings />
-      <ViewAllJobs />
-
-    </>
+    <RouterProvider router={router} />
   )
 }
 
