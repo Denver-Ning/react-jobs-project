37dcfacdfeec7d7263cbbdb6e89a3d6dc5ffc85b
zhangshuaining <1208505019@qq.com>
2024-04-10 22:46:32 +0800

    chore: test json server

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   server:{
     port: 3000,
     proxy:{
       '/api': {
-        target: 'https://json-server-vercel-xi-puce.vercel.app/',
+        target: 'https://json-server-vercel-xi-puce.vercel.app',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
