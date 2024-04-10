99235e5a5a28f43e35ecdf931f181404723f3b86
zhangshuaining <1208505019@qq.com>
2024-04-10 23:01:09 +0800

    fix: package

--- api/server.js
+++ api/server.js
@@ -1,20 +1,19 @@
-const jsonServer = require('json-server')
+import jsonServer from 'json-server';
+import { createRequire } from 'module';
 
-const server = jsonServer.create()
+const require = createRequire(import.meta.url);
+const server = jsonServer.create();
+const router = jsonServer.router('jobs.json');
+const middlewares = jsonServer.defaults();
 
-
-const router = jsonServer.router('jobs.json')
-
-const middlewares = jsonServer.defaults()
-
-server.use(middlewares)
+server.use(middlewares);
 server.use(jsonServer.rewriter({
     '/api/*': '/$1',
-}))
-server.use(router)
+}));
+server.use(router);
 server.listen(8000, () => {
-    console.log('JSON Server is running')
-})
+    console.log('JSON Server is running');
+});
 
 // Export the Server API
-module.exports = server
+export default server;
