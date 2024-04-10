f7b382072941698b2f1dfb58012e6451413fd4d8
zhangshuaining <1208505019@qq.com>
2024-04-10 16:43:31 +0800

    feat: add addJob page

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,28 @@ import HomePage from "./pages/HomePage"
 import MainLayouts from './layouts/MainLayouts'
 import JobsPage from './pages/JobsPage'
 import NotFoundPage from './pages/NotFoundPage'
+import AddJobPage from './pages/AddJobPage'
+import { JobType } from './types/Job'
 
 const App = () => {
+
+  const addJob = async (job:JobType) => {
+    await fetch(`/api/jobs`,{
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(job)
+    })
+    return;
+  }
+
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<MainLayouts />}>
         <Route index element={<HomePage />} />
-        <Route path='jobs' element={<JobsPage />} />
+        <Route path='/jobs' element={<JobsPage />} />
+        <Route path='/add-job' element={<AddJobPage addJobSubmit={addJob}/>} />
         <Route path='*' element={<NotFoundPage />} />
       </Route>
     )
