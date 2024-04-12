802a2a254417cc6714b59d8be793d13a278a5644
zhangshuaining <1208505019@qq.com>
2024-04-12 14:35:45 +0800

    chore: 区分环境proxy地址

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+declare interface ImpoertMetaEnv {
+  readonly MODE: string
+  // readonly VITE_BASE_URL: string
+  // readonly VITE_TIME_OUT: number
+}
+ 
+declare interface ImpoertMeta {
+  readonly env: ImpoertMetaEnv
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,48 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/',
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src'),
-    }
-  },
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'https://react-jobs-gamma.vercel.app/',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
+
+export default (({ mode }: { mode: string }) => {
+  // process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
+  const API_BASE_URL = process.env.VITE_API_BASE_URL;
+  return defineConfig({
+    plugins: [react()],
+    base: '/',
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, 'src'),
       }
     },
-  }
+    server: {
+      port: 3000,
+      proxy: {
+        '/api': {
+          target: API_BASE_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '')
+        }
+      },
+    }
+  })
 })
+
+// https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '/',
+//   resolve: {
+//     alias: {
+//       '@': resolve(__dirname, 'src'),
+//     }
+//   },
+//   server: {
+//     port: 3000,
+//     proxy: {
+//       '/api': {
+//         target: 'http://localhost:8000',
+//         changeOrigin: true,
+//         rewrite: (path) => path.replace(/^\/api/, '')
+//       }
+//     },
+//   }
+// })
