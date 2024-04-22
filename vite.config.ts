8db30f97d1ee8e0dda4f975b45fd495bb93f27fc
zhangshuaining <1208505019@qq.com>
2024-04-22 20:25:21 +0800

    fix: reactjob vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
-import { defineConfig } from 'vite'
+import { defineConfig,loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
 
 export default (({ mode }: { mode: string }) => {
-  // process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
   const API_BASE_URL = process.env.VITE_API_BASE_URL;
+  console.log('API_BASE_URL', API_BASE_URL);
+
   return defineConfig({
     plugins: [react()],
     base: '/',
